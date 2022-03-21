8728e3804b96e16305a3e2e18b1bc3af2b430fc9
@betagon <gon.fervic@gmail.com>
2022-03-21 22:49:09 +0000

    finished testimonial component

---
+++ src/front/js/component/testimonial.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import Avatar from "../../img/avatar.jpg";
+
+const Testimonial = () => {
+  return (
+    <>
+      <div className="cotainer my-5 bck-light-blue">
+        <div className="testimonial d-flex flex-column align-items-center">
+          <p className="quote ">
+            No Zig’s save me a lot of time. It gave me the perfect route for all
+            the places I wanted to visit{" "}
+          </p>
+          <img className="testimonial-img" src={Avatar} />
+          <p className="quote-name">Bruno Marques</p>
+          <p className="quote-desc">Avid traveller</p>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Testimonial;
