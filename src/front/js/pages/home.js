2695769c2a7db9afd279e896ba7b30cbcd597efa
KrisEng123 <kristalleng@outlook.de>
2022-03-25 19:12:44 +0000

    addedJoinNow

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -7,6 +7,7 @@ import Hero from "../component/hero.jsx";
 import Steps from "../component/steps.jsx";
 import SecondHero from "../component/second_hero.jsx";
 import Testimonial from "../component/testimonial.jsx";
+import JoinNow from "../component/joinNow.jsx";
 
 export const Home = () => {
   const { store, actions } = useContext(Context);
@@ -25,6 +26,7 @@ export const Home = () => {
       <Steps />
       <SecondHero />
       <Testimonial />
+      <JoinNow />
     </div>
   );
 
