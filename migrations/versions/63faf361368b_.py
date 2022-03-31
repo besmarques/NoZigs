e42fb75afcb246d9ccfb41dd9e9eb951b416389b
Bruno Marques <besmarques@gmail.com>
2022-03-31 10:15:43 +0000

    signup working

--- migrations/versions/63faf361368b_.py
+++ migrations/versions/63faf361368b_.py
@@ -1,8 +1,8 @@
 """empty message
 
-Revision ID: 9259adbcbecd
+Revision ID: 63faf361368b
 Revises: 
-Create Date: 2022-03-29 16:14:25.388648
+Create Date: 2022-03-31 10:07:19.797906
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = '9259adbcbecd'
+revision = '63faf361368b'
 down_revision = None
 branch_labels = None
 depends_on = None
