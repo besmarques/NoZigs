0f0bf8caa2046a2e75ee40d657a3634c8eed43c9
@betagon <gon.fervic@gmail.com>
2022-04-04 19:22:25 +0000

    navbar modifications v.2

--- src/front/js/component/navbar.jsx
+++ src/front/js/component/navbar.jsx
@@ -2,6 +2,7 @@ import React, { useContext } from "react";
 import { Context } from "../store/appContext";
 import { Link, useLocation } from "react-router-dom";
 import Logo from "../../img/logo.png";
+import "../../styles/navbar.css";
 
 export const Navbar = () => {
     const { store, actions } = useContext(Context);
@@ -9,8 +10,16 @@ export const Navbar = () => {
     let location = useLocation();
     console.log(location.pathname);
 
+    const display =
+        location.pathname === "/" ||
+        location.pathname === "/login" ||
+        location.pathname === "/signup"
+            ? "display-none"
+            : "";
+
+    console.log(display);
     return (
-        <nav className="navbar navbar-light bg-light">
+        <nav className="navbar py-4">
             <div className="container">
                 <Link to="/">
                     <span className="navbar-brand mb-0 h1 text-danger">
@@ -18,40 +27,33 @@ export const Navbar = () => {
                     </span>
                 </Link>
 
-                <Link to="/trips">
-                    <span className="navbar-brand mb-0 h1 text-danger">
-                        Trips
-                    </span>
-                </Link>
-
-                <Link to="/profile">
-                    <span className="navbar-brand mb-0 h1 text-danger">
-                        Profile
-                    </span>
-                </Link>
-
                 <div className="ml-auto">
-                    {!store.token ? (
-                        <Link to="/login">
-                            <button className="btn btn-info text-white">
-                                Login
-                            </button>
+                    <div className="d-flex align-items-center justify-content-around">
+                        <Link to="/trips" className="link">
+                            <span className={`${display}`}>Trips</span>
                         </Link>
-                    ) : (
-                        <Link to="/login" onClick={() => actions.logout()}>
-                            <button className="btn btn-info text-white">
-                                Logout
-                            </button>
-                        </Link>
-                    )}
 
-                    {!store.token && (
-                        <Link to="/signup">
-                            <button className="btn btn-outline-info">
-                                Signup
-                            </button>
+                        <Link to="/profile" className="link">
+                            <span className={`${display}`}>Profile</span>
                         </Link>
-                    )}
+                        {!store.token ? (
+                            <Link to="/login">
+                                <button className="login-link">Login</button>
+                            </Link>
+                        ) : (
+                            <Link to="/login" onClick={() => actions.logout()}>
+                                <button className="btn btn-info text-white">
+                                    Logout
+                                </button>
+                            </Link>
+                        )}
+
+                        {!store.token && (
+                            <Link to="/signup">
+                                <button className="signupu">Signup</button>
+                            </Link>
+                        )}
+                    </div>
                 </div>
             </div>
         </nav>
